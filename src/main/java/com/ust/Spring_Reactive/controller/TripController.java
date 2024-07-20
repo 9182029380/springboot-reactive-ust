aa950bd28a286f4d1ead6ba1d97cdae56f3f31dc
9182092380 <pothulasharath452@gmail.com>
2024-07-20 10:17:25 +0530

    secound-commit

--- src/main/java/com/ust/Spring_Reactive/controller/TripController.java
+++ src/main/java/com/ust/Spring_Reactive/controller/TripController.java
@@ -3,10 +3,8 @@ package com.ust.Spring_Reactive.controller;
 import com.ust.Spring_Reactive.dto.Tripdto;
 import com.ust.Spring_Reactive.service.TripService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -19,5 +17,22 @@ public class TripController {
         public Mono<Tripdto> addTrip(@RequestBody Mono<Tripdto> tripdtomono){
             return tripService.addTrip(tripdtomono);
         }
+    @GetMapping("/gettrips")
+    public Flux<Tripdto> getAllTrips(){
+        return tripService.getAllTrips();
+    }
+
+    @GetMapping("/gettrip/{id}")
+    public Mono<Tripdto> getTripById(@PathVariable String id){
+        return tripService.getTripById(id);
+    }
+    @PutMapping("/updatetrip/{id}")
+    public Mono<Tripdto> updateTrip(@PathVariable String id, @RequestBody Mono<Tripdto> tripdtomono){
+        return tripService.updateTrip(id, tripdtomono);
+    }
+    @DeleteMapping("/deletetrip/{id}")
+    public Mono<Void> deleteTrip(@PathVariable String id){
+        return tripService.deleteTrip(id);
+    }
 
 }
--- src/main/java/com/ust/Spring_Reactive/service/TripService.java
+++ src/main/java/com/ust/Spring_Reactive/service/TripService.java
@@ -6,6 +6,7 @@ import com.ust.Spring_Reactive.dto.Tripdto;
 import com.ust.Spring_Reactive.utils.AppUtlis;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -18,5 +19,25 @@ public class TripService {
         return tripdtomono.map(AppUtlis::dtoToEntity)
                           .flatMap(repo::insert)
                 .map(AppUtlis::entityToDto);
+    }
+
+    public Flux<Tripdto> getAllTrips() {
+        return repo.findAll().map(AppUtlis::entityToDto);
+    }
+
+    public Mono<Tripdto> getTripById(String id) {
+        return repo.findById(id).map(AppUtlis::entityToDto);
+    }
+
+    public Mono<Tripdto> updateTrip(String id, Mono<Tripdto> tripdtomono) {
+        return repo.findById(id)
+                .flatMap(p->tripdtomono.map(AppUtlis::dtoToEntity)
+                .doOnNext(e->e.setId(id))
+                .flatMap(repo::save)
+                .map(AppUtlis::entityToDto));
+    }
+
+    public Mono<Void> deleteTrip(String id) {
+        return repo.deleteById(id);
     }
 }
